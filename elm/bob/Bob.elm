3c7d25a4e00a2188107c057773e96ccc55a09e3b
Daniel Chong <danielgq.chong@gmail.com>
2016-10-03 09:36:08 +1300

    added elm code

--- elm/bob/Bob.elm
+++ elm/bob/Bob.elm
@@ -1,1 +1,25 @@
 module Bob exposing (..)
+import String exposing (..)
+
+hey: String -> String
+hey something =
+    if isSilent something then
+        "Fine. Be that way!"
+    else if isShouting something then
+        "Whoa, chill out!"
+    else if isQuestion something then
+        "Sure."
+    else
+        "Whatever."
+
+isQuestion: String -> Bool
+isQuestion something =
+    String.endsWith "?" something
+
+isShouting: String -> Bool
+isShouting something =
+    String.toUpper something == something && String.toLower something
+
+isSilent: String -> Bool
+isSilent something =
+    String.trim something == ""
